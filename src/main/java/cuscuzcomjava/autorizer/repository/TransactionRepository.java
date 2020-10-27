e3b830353fe1f9d79d3455a3b6c8976e0e09a7a6
DeMesquita <nayaramesquita2323@gmail.com>
2020-10-27 11:16:36 -0300

    add AccountRepository and TransactionRepository

--- src/main/java/cuscuzcomjava/autorizer/repository/TransactionRepository.java
+++ src/main/java/cuscuzcomjava/autorizer/repository/TransactionRepository.java
@@ -1,2 +1,10 @@
-package cuscuzcomjava.autorizer.repository;public interface TransactionRepository {
+package cuscuzcomjava.autorizer.repository;
+
+import cuscuzcomjava.autorizer.entity.Account;
+import cuscuzcomjava.autorizer.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 }
