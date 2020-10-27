b7e1c7600da5b7874d8fb6e797c6f9ba74a4a5ee
Leonardo A. de Souza <leo26gpl@gmail.com>
2020-10-26 22:24:46 -0300

    Add Account Entity
    Created with autogenerated id, and activeCard and availableLimit columns with spring notations.

---
+++ src/main/java/cuscuzcomjava/autorizer/entity/Account.java
@@ -0,0 +1,37 @@
+package cuscuzcomjava.autorizer.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+
+  @Id
+  @GeneratedValue
+  @Column
+  private int id;
+
+  @Column
+  private boolean activeCard;
+
+  @Column
+  private int availableLimit;
+
+  public boolean isActiveCard() {
+    return activeCard;
+  }
+
+  public void setActiveCard(boolean activeCard) {
+    this.activeCard = activeCard;
+  }
+
+  public int getAvailableLimit() {
+    return availableLimit;
+  }
+
+  public void setAvailableLimit(int availableLimit) {
+    this.availableLimit = availableLimit;
+  }
+}
